040d1302786f638e72cae022535f5bb837385a98
kayapati vaishnavi <vaishnavikayapati@gmail.com>
2026-03-05 16:43:00 +0530

    Added Java programs

---
+++ class Reverse of array{.java
@@ -0,0 +1,8 @@
+class Reverseofarray{ 
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
